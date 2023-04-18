6ffe3398a6f1f6aabcc8bc10d82dbcc3c8809249
nate-d3v <nate.d3v@gmail.com>
2023-04-17 23:52:06 -0300

    Setup the repo

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				chromate: ['Chromate', 'serif'],
+				comfortaa: ['Comfortaa', 'cursive'],
+				'work-sans': ['Work Sans', 'sans-serif'],
+			},
+			colors: {
+				neutral: '#F5F5F5',
+				primary: '#FF6F61',
+				accent: '#00A4FF',
+			},
+		},
+	},
+	plugins: [],
+};
